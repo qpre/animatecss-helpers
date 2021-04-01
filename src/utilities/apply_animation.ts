e9396145d4c7c080b7b9a726c5bdf9c617ae55e2
Quentin Pré <pre.quentin@gmail.com>
2021-04-01 09:25:00 +0200

    [FIX] default param for node list

--- src/utilities/apply_animation.ts
+++ src/utilities/apply_animation.ts
@@ -1,6 +1,6 @@
 const sleep = (duration: number) => new Promise(r => setTimeout(r, duration));
 
-export function apply_animation({ animation_class, nodes, duration = 1, delay = 0, stagger = 0 }: { animation_class: string, nodes: Array<HTMLElement>, duration: number, delay: number, stagger: number }) {
+export function apply_animation({ animation_class = '', nodes = [], duration = 1, delay = 0, stagger = 0 }: { animation_class: string, nodes: Array<HTMLElement>, duration: number, delay: number, stagger: number }) {
   const apply_async = async function() {
     if (delay) {
       await sleep(delay);
