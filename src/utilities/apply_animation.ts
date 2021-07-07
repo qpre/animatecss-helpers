9cd0c265e8b3552bd4d3143936621c079ea36888
Quentin Pré <pre.quentin@gmail.com>
2021-07-07 12:43:33 +0200

    [UPD] no more async await, tempo is applied via css

--- src/utilities/apply_animation.ts
+++ src/utilities/apply_animation.ts
@@ -1,21 +1,8 @@
-const sleep = (duration: number) => new Promise(r => setTimeout(r, duration));
-
 export function apply_animation({ animation_class = '', nodes = [], duration = 1, delay = 0, stagger = 0 }: { animation_class: string, nodes: Array<HTMLElement>, duration: number, delay: number, stagger: number }) {
-  const apply_async = async function() {
-    if (delay) {
-      await sleep(delay);
-    }
-
-    for (const node of nodes) {
-        node?.classList?.add('animate__animated');
-        node?.style.setProperty('--animate-duration', `${duration}s`);
-        node?.classList?.add(animation_class);
-
-        if (stagger) {
-          await sleep(stagger);
-        }
-    }
+  for (const node of nodes) {
+    node?.classList?.add('animate__animated');
+    node?.style.setProperty('--animate-duration', `${duration}s`);
+    node?.style.setProperty('--animate-delay', `${delay * (stagger > 0 ? nodes.indexOf(node) + 1 : 1)}s`);
+    node?.classList?.add(animation_class);
   }
-  
-  return apply_async();
 }
